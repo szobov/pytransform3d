b81af6723028758e00019090ab0f49b0aa8ffc58
Sergei Zobov <sergei.zobov@micropsi-industries.com>
2023-07-04 13:54:52 +0200

    pytransform3d/visualizer/__init__.py: use new optional imports mechanism

--- pytransform3d/visualizer/__init__.py
+++ pytransform3d/visualizer/__init__.py
@@ -1,6 +1,11 @@
 """Optional 3D renderer based on Open3D's visualizer."""
-import warnings
-try:
+from ..compatibility import (
+    opend3d_module,
+    import_optional_dependency,
+    OptionalImportErrorHandling,
+)
+
+if import_optional_dependency(opend3d_module, error_handling=OptionalImportErrorHandling.RAISE):
     import open3d as o3d
     from ._artists import (Artist, Line3D, PointCollection3D, Vector3D, Frame,
                            Trajectory, Camera, Box, Sphere, Cylinder, Mesh,
@@ -11,5 +16,3 @@ try:
                "Vector3D", "Frame", "Trajectory", "Camera", "Box", "Sphere",
                "Cylinder", "Mesh", "Ellipsoid", "Capsule", "Cone", "Plane",
                "Graph"]
-except ImportError:
-    warnings.warn("3D visualizer is not available. Install open3d.")
